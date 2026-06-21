a7b9c7b27fd217f9e65c47443e3166eed1f8f03b
Halopolicj <xxcien.smiercixx@gmail.com>
2026-06-21 17:07:11 +0200

    forgor attributes

---
+++ src/ppm/content/PermaAttr.java
@@ -0,0 +1,12 @@
+package ppm.content;
+
+import mindustry.world.meta.*;
+
+public class PermaAttr {
+
+    public static void load() {
+        Attribute.add("Nickel");
+        Attribute.add("Gallium");
+
+    }
+}
