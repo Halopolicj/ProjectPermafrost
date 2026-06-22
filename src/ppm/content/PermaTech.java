947bc5b4cd2b6aca08d91131bf46b1b1ddd00c50
Halopolicj <xxcien.smiercixx@gmail.com>
2026-06-22 15:16:44 +0200

    fuh

--- src/ppm/content/PermaTech.java
+++ src/ppm/content/PermaTech.java
@@ -3,8 +3,8 @@ package ppm.content;
 public class PermaTech {
 
     public static void load() {
-        TechTemperate.load();
         TechCold.load();
         TechHot.load();
+        TechTemperate.load();
     }
 }
