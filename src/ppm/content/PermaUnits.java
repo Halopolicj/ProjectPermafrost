415da8bd94d2e2234c89713fd9458284be9901ee
Halopolicj <xxcien.smiercixx@gmail.com>
2026-06-28 02:32:51 +0200

    blue

--- src/ppm/content/PermaUnits.java
+++ src/ppm/content/PermaUnits.java
@@ -37,7 +37,7 @@ public class PermaUnits {
             legStraightness = .6f;
             //legBaseOffset = 2.5f;
             legPairOffset = 2.5f;
-            rippleScale = 0f;
+            rippleScale = 0.01f;
             lockLegBase = true;
             legContinuousMove = true;
             legPhysicsLayer = true;
