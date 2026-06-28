817a82ba0f29e71b6f8a7bb1714089a670df4c31
Halopolicj <xxcien.smiercixx@gmail.com>
2026-06-28 02:29:17 +0200

    blue

--- src/ppm/content/PermaSectors.java
+++ src/ppm/content/PermaSectors.java
@@ -10,17 +10,7 @@ public class PermaSectors {
     public static SectorPreset one, crossingIslands;
 
     public static void load() {
-        one = new SectorPreset("one", PermaPlanets.Gleo , 21)
-        {{
-            alwaysUnlocked = true;
-            addStartingItems = true;
-            captureWave = 10;
-            difficulty = 1;
-            noLighting = true;
-            startWaveTimeMultiplier = 3f;
-            requireUnlock = false;
 
-        }};
 
         crossingIslands = new SectorPreset("crossing-IslandsV1", Gleo, 15){{
            alwaysUnlocked = true;
